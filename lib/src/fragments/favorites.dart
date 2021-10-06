ac1253d02378b8db6f5fb0178bf41078d3c8dc57
DESKTOP-JGGKFJ7\Rajat <rajat.k@surya-soft.com>
2021-10-06 19:15:22 +0530

    Add bottom tabs

---
+++ lib/src/fragments/favorites.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Favorites extends StatelessWidget {
+  Widget build(context) {
+    return Center(
+      child: Text(''),
+    );
+  }
+}
---
+++ lib/src/fragments/search.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  Widget build(context) {
+    return Center(
+      child: Text(''),
+    );
+  }
+}
