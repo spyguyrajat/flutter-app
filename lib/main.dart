5577a0d7108765b8d629d169302deaa8454db0e0
DESKTOP-JGGKFJ7\Rajat <rajat.k@surya-soft.com>
2021-10-11 16:07:21 +0530

    file name issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'src/homePage.dart';
+import 'src/homepage.dart';
 import 'src/theme/appTheme.dart';
 
 void main() {
